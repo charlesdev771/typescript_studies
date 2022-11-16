d1513846d57d4d459fbc92e6a75eaa4f5bb33f1a
charlesdev771 <charlesdev07721@proton.me>
2022-11-15 22:56:36 -0300

    aa

---
+++ dist/js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const nb = 17;
+const myName = "Charles";
+console.log(myName);
+const user = {
+    name: "Charles",
+    age: 21,
+};
+let myName2 = "charles";
+function sayHello(msg) {
+    console.log(msg);
+}
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+const nb: number = 17;
+
+const myName: string = "Charles";
+
+console.log(myName);
+
+const user: {name: string, age: number} = {
+    name: "Charles",
+    age: 21,
+};
+
+let myName2: string | number = "charles";
+
+function sayHello(msg: string): void
+{
+    console.log(msg);
+}
